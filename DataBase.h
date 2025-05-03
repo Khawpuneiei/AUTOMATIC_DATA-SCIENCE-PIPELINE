85f292b2371e04502c6fc92245274b059509779f
Khawpuneiei <116812879+Khawpuneiei@users.noreply.github.com>
2025-05-03 21:58:55 +0700

    Update DataBase.h
    add include

--- DataBase.h
+++ DataBase.h
@@ -1,4 +1,10 @@
-
+#include <iostream>  
+#include <fstream>               
+#include <sstream>               
+#include <unordered_map>         
+#include <vector>                
+#include <string>
+using namespace std;
 void searchKey(const unordered_map<string, unordered_map<string, string>>& hashTable, const string& key);
 void deleteKey(unordered_map<string, unordered_map<string, string>>& hashTable, const string& key);
 void insertKey(unordered_map<string, unordered_map<string, string>>& hashTable,
