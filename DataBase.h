7e812a474ac7370d4ea194b58f3c5ed2a4ed43c4
Khawpuneiei <116812879+Khawpuneiei@users.noreply.github.com>
2025-05-03 21:41:11 +0700

    Create DataBase.h

---
+++ DataBase.h
@@ -0,0 +1,8 @@
+
+void searchKey(const unordered_map<string, unordered_map<string, string>>& hashTable, const string& key);
+void deleteKey(unordered_map<string, unordered_map<string, string>>& hashTable, const string& key);
+void insertKey(unordered_map<string, unordered_map<string, string>>& hashTable,
+               const string& key,
+               const unordered_map<string, string>& values);
+
+void printHashTable(const unordered_map<string, unordered_map<string, string>>& hashTable);
