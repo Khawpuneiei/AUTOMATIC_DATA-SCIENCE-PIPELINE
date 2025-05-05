7c2a18fea7ce9b25f48cb98e65eabf449f6fc39f
Khawpuneiei <116812879+Khawpuneiei@users.noreply.github.com>
2025-05-05 20:31:22 +0700

    Update DataBase.h

--- DataBase.h
+++ DataBase.h
@@ -1,14 +1,32 @@
-#include <iostream>  
-#include <fstream>               
-#include <sstream>               
-#include <unordered_map>         
-#include <vector>                
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
 #include <string>
 using namespace std;
-void searchKey(const unordered_map<string, unordered_map<string, string>>& hashTable, const string& key);
-void deleteKey(unordered_map<string, unordered_map<string, string>>& hashTable, const string& key);
-void insertKey(unordered_map<string, unordered_map<string, string>>& hashTable,
-               const string& key,
-               const unordered_map<string, string>& values);
 
-void printHashTable(const unordered_map<string, unordered_map<string, string>>& hashTable);
+class Table {
+private:
+    unordered_map<string, unordered_map<string, string>> data;
+
+public:
+    void insert(const string& key, const unordered_map<string, string>& row);
+
+    void search(const string& key) const;
+
+    void remove(const string& key);
+
+    void printAll() const;
+
+    static Table loadFromCSV(const string& filename);
+
+    const unordered_map<string, unordered_map<string, string>>& getData() const;
+};
+
+vector<string> split(const string& line);
+
+#endif
